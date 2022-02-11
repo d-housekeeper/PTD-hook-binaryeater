ebffa7a86a21d45519049042db068794dbc5d38b
d-housekeeper <97742132+d-housekeeper@users.noreply.github.com>
2022-02-12 02:19:09 +0900

    Rename ptd-tool project name

--- ptd-tool/default.nix
+++ ptd-tool/default.nix
@@ -10,7 +10,7 @@ buildGoApplication rec {
   buildInputs = [ jq ];
   nativeBuildInputs = [ unzip ];
   src = fetchurl {
-    url = "https://github.com/d-housekeeper/ptd-tool-binaryeater/archive/${version}.zip";
+    url = "https://github.com/d-housekeeper/ptd-tool/archive/${version}.zip";
     sha256 = "10ynipxrfyyhf7s66a4k5r6gi8qva677gyj2vyjcmw4x59vq67bv";
   };
   modules = ./gomod2nix.toml;
